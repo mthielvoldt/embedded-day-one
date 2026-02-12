4c78ff993a35be5b3dac3607f2dca0fe58573a24
Mike T <mthielvoldt@gmail.com>
2026-02-12 13:41:46 -0500

    Unit tests build

---
+++ test/fakes/serial/serial.h
@@ -0,0 +1,15 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+/**
+ * Contains a subset of the #defines typedefs and function declarations that are in the acutal serial.h
+ * This file narrows the scope of what we need to provide fake implementations for to just those
+ * functions that are actually used by the code under test.
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+
+#endif // SERIAL_H
